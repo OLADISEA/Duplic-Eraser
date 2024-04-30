33bc1aa152d78616b809da602dbc0bf4b8019864
OLADISEA <ladisea55@gmail.com>
2024-04-30 08:44:45 +0100

    "Implemented user input page with bloc integration for input validation and navigation to the RemoveDuplicate screen upon successful input

--- lib/screens/user_input/user_input_page.dart
+++ lib/screens/user_input/user_input_page.dart
@@ -1,4 +1,3 @@
-import 'package:duplic_eraser/common/widgets/widget.dart';
 import 'package:duplic_eraser/screens/bloc/input_bloc.dart';
 import 'package:duplic_eraser/screens/bloc/input_events.dart';
 import 'package:flutter/material.dart';
@@ -6,69 +5,90 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
+import '../../components/widgets/widget.dart';
 import '../bloc/input_states.dart';
 import '../duplicate_remover/remover_duplicate.dart';
 
 class UserInputPage extends StatelessWidget {
   UserInputPage({super.key});
 
+  // Text editing controller and focus node for input field
   final TextEditingController controller = TextEditingController();
+  FocusNode focusNode = FocusNode();
+
+  // Error text to display validation errors
   String errorText = "";
 
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.init(context);
     return Scaffold(
-      body: Container(
-        child: BlocConsumer<InputBloc,InputState>(
+      body: BlocConsumer<InputBloc,InputState>(
+        builder: (BuildContext context, state) {
+          return Container(
+            margin: EdgeInsets.only(top: 100.h,left: 39.w,right: 34.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Title and logo
+                titlePage(text: "Duplic Eraser", image: "assets/logo/duplic_logo.png"),
+                SizedBox(height: 120.h,),
 
-          builder: (BuildContext context, state) {
-            return Container(
-              margin: EdgeInsets.only(top: 50.h,left: 24.w,right: 15.w),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  reusableText(text: "Welcome to String Duplicate Remover",fontSize: 22.sp,fontWeight: FontWeight.w700),
-                  SizedBox(height: 150.h,),
-                  //SizedBox(height: 24.h,),
-                  reusableText(text: "Input Field",fontSize: 14.sp),
-                  textField(text: "Enter alphabet characters only", controller: controller, onChangedValue: () {
-                    print(controller.text);
-                    context.read<InputBloc>().add(ConfirmAlphabetEvents(characters: controller.text));
-                  }),
-                  reusableText(text: errorText,color: Colors.red),
-                  SizedBox(height: 30.h,),
-                  generalButton(text: "Submit", onTap: (){
-                    context.read<InputBloc>().add(SubmitButtonPressed(input: controller.text));
-                    //Navigator.of(context).push(MaterialPageRoute(builder: (context)=> RemoveDuplicate()));
-                  })
-                ],
-              ),
+                reusableText(text: "Input Field",fontSize: 14.sp),
+
+                // Input field widget
+                textField(
+                    text: "Enter alphabet characters only",
+                    controller: controller,
+                    focusNode: focusNode,
+                    onChangedValue: () {
+
+                      // Notify bloc to confirm alphabet characters only
+                      context.read<InputBloc>().add(ConfirmAlphabetEvents(characters: controller.text.toLowerCase()));
+                    }),
+                SizedBox(height: 5.h,),
+                // Error text display
+                reusableText(text: errorText,color: Colors.red),
+                SizedBox(height: 20.h,),
+              // Submit button
+              generalButton(text: "Submit", onTap: (){
+
+                // Notify bloc that submit button is pressed
+                context.read<InputBloc>().add(SubmitButtonPressed(input: controller.text));
+              })
+            ]
+          ));
+        },
+        listener: (BuildContext context, Object? state) {
+          // If input is successful, navigate to RemoveDuplicate screen
+          if(state is InputSuccess){
+            String userInput = controller.text;
+            controller.clear();
+            focusNode.unfocus();
+            Navigator.of(context).push(MaterialPageRoute(builder: (context)=> RemoveDuplicate(originalString: userInput,)));
+
+          }
+          // Show toast message if input is empty
+          if(state is InputError && state.emptyStringError.isNotEmpty){
+            Fluttertoast.showToast(
+                msg: state.emptyStringError,
+                toastLength: Toast.LENGTH_SHORT,
+                gravity: ToastGravity.BOTTOM,
+                timeInSecForIosWeb: 1,
+                backgroundColor: Colors.blue,
+                textColor: Colors.white,
+                fontSize: 12.sp
             );
-          },
-          listener: (BuildContext context, Object? state) {
-            if(state is InputSuccess){
-              Navigator.of(context).push(MaterialPageRoute(builder: (context)=> RemoveDuplicate()));
-
-            }
-            if(state is InputError && state.emptyStringError.isNotEmpty){
-              Fluttertoast.showToast(
-                  msg: "An error occurred: ${state.emptyStringError}",
-                  toastLength: Toast.LENGTH_SHORT,
-                  gravity: ToastGravity.CENTER,
-                  timeInSecForIosWeb: 1,
-                  backgroundColor: Colors.blue,
-                  textColor: Colors.white,
-                  fontSize: 16.0
-              );
-            }
-            if(state is InputError && state.alphabetError.isNotEmpty){
-              errorText = "The Field must consist of only alphabets";
-            }
-            if(state is InputError && state.alphabetError.isEmpty){
-              errorText = "";
-            }
-          },
-        ),
+          }
+          // Show error message if input contains non-alphabet characters
+          if(state is InputError && state.alphabetError.isNotEmpty){
+            errorText = "The Field must consist of only alphabets";
+          }
+          // Clear error text if input is valid
+          if(state is InputError && state.alphabetError.isEmpty){
+            errorText = "";
+          }
+        },
       ),
     );
   }
