485803ff6fdc4c35a057fa15ad0a4bb74d08f0a2
OLADISEA <ladisea55@gmail.com>
2024-04-30 08:37:25 +0100

    Initialize the app with necessary database setup and Bloc providers for input and input display management

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,33 @@
 import 'package:duplic_eraser/screens/bloc/input_bloc.dart';
+import 'package:duplic_eraser/screens/input_display/bloc/input_display_bloc.dart';
 import 'package:duplic_eraser/screens/user_input/user_input_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main(){
-  runApp(
+import 'database/user_input_db.dart';
 
-      BlocProvider(
-          create: (context) => InputBloc(),
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await UserInputDatabase.initialize();
+  runApp(
+      MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => InputBloc(userInputDatabase: UserInputDatabase()),),
+          BlocProvider(create: (context) => InputDisplayBloc(userInputDatabase: UserInputDatabase()),)
+        ],
           child: const MyApp()));
 }
 
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    //Using screen util for responsiveness
     return ScreenUtilInit(
+      designSize: const Size(360, 690), // design size
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         home: UserInputPage(),
